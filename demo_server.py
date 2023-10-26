7744b65ec158d5060375c59ce8a7507090b3dba6
Liyulingyue <852433440@qq.com>
2023-10-26 21:28:43 +0800

    Changes

--- demo_server.py
+++ demo_server.py
@@ -9,6 +9,9 @@ import random
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://yiyan.baidu.com"}})
 
+with open("answersbook.json", "r", encoding='utf-8') as f:
+    answerBook = json.load(f)
+
 wordbook = []
 
 def make_json_response(data, status_code=200):
@@ -16,6 +19,18 @@ def make_json_response(data, status_code=200):
     response.headers["Content-Type"] = "application/json"
     return response
 
+
+@app.route("/findanswer", methods=['POST'])
+async def findanswer():
+    """
+        输入一个数字，查询答案之书的内容
+    """
+    num = request.json.get('number', "")
+    if int(num) < 1 or int(num) > 268:
+        return make_json_response({"message": "超出了答案之书的页码范围，请重新输入"})
+    answer = answerBook[str(num)]
+    prompt = "答案之书中该数字对应的内容是(message)，根据答案之书中的答案(message)，生成一段不超过100字的含义解释"
+    return make_json_response({"message": answer, "prompt":prompt})
 
 @app.route("/add_word", methods=['POST'])
 async def add_word():
@@ -99,4 +114,4 @@ def index():
     return 'welcome to my webpage!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8081)
+    app.run(debug=True, host='127.0.0.1', port=8082)
